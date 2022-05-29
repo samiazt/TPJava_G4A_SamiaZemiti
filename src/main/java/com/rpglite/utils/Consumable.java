2e00285f80fc9945822a025e7234d2876c3ed519
samour <93775064+samiazt@users.noreply.github.com>
2022-05-29 23:39:31 +0200

    Delete src directory

--- src/main/java/com/rpglite/utils/Consumable.java
+++
@@ -1,7 +0,0 @@
-package com.rpglite.utils;
-
-public interface Consumable {
-}
-
-// Interface Consumable des classes Potion et Food
-// qui sert aux héros d'utiliser un consommable lors des combats (pas encore codé)
